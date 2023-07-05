7d4e57a5d0f3428d3479b35dee8f591101b318d0
christopher cuzin <ed.ed1009@hotmail.fr>
2023-07-05 12:35:59 +0200

    WOB_new_file_request add file resolver to apollo schema

--- backend/src/Utils/createApolloSchema.ts
+++ backend/src/Utils/createApolloSchema.ts
@@ -1,11 +1,12 @@
-import { buildSchema } from "type-graphql";
-import { AuthResolver } from "../resolvers/AuthResolver";
+import { buildSchema } from "type-graphql"
+import { AuthResolver } from "../resolvers/AuthResolver"
+import { FileResolver } from "../resolvers/FilesResolver"
 
-export const createApolloSchema = async(): Promise<any> =>
+export const createApolloSchema = async (): Promise<any> =>
   await buildSchema({
-    resolvers: [AuthResolver],
+    resolvers: [AuthResolver, FileResolver],
     authChecker: ({ context }) => {
       // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-      return !!context.user;
-    },
-  });
+      return !!context.user
+    }
+  })
