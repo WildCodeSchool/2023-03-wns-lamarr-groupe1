4ee365bf13c886f774128d18fdf59707b69d541d
christopher cuzin <ed.ed1009@hotmail.fr>
2023-07-06 16:53:56 +0200

    WOB_crud_subscription modify crud

--- backend/src/resolvers/AuthResolver.ts
+++ backend/src/resolvers/AuthResolver.ts
@@ -1,9 +1,10 @@
-import { Arg, Authorized, Ctx, Mutation, Query } from "type-graphql";
-import { UsersModels } from "../models/UsersModels";
-import * as argon2 from 'argon2'
-import { sign } from "jsonwebtoken";
-import { SignUpInput } from "../inputs/SignUpInput";
+import { Arg, Authorized, Ctx, Mutation, Query } from "type-graphql"
+import { UsersModels } from "../models/UsersModels"
+import * as argon2 from "argon2"
+import { sign } from "jsonwebtoken"
+import { SignUpInput } from "../inputs/SignUpInput"
 import { SubscriptionModels } from "../models/SubscriptionModels"
+import { calculateEndedAt } from "../services/SubcriptionEndedAt.service"
 
 export class AuthResolver {
   // Mutation signUp -> insérer un utilisateur en BDD (à partir d'identifiants)
@@ -11,9 +12,32 @@ export class AuthResolver {
   @Mutation(() => String)
   async signUp(
     @Arg("input")
-    { firstname, lastname, username, password, email }: SignUpInput
+    { firstname, lastname, username, password, email, type }: SignUpInput
   ): Promise<string> {
-    const hashedPassword = await argon2.hash(password);
+    const hashedPassword = await argon2.hash(password)
+    const subscribedAt = new Date()
+    let subscription: SubscriptionModels
+    type = type.charAt(0).toUpperCase() + type.substring(1).toLowerCase()
+
+    if (type === "Expert") {
+      const status = "Active"
+      const duration = "Monthly"
+      const subscriptionEndedAt = calculateEndedAt(
+        duration,
+        status,
+        subscribedAt
+      )
+      subscription = await SubscriptionModels.create({
+        status,
+        duration,
+        subscriptionEndedAt,
+        subscribedAt,
+        type
+      }).save()
+    } else {
+      const status = "Inactive"
+      subscription = await SubscriptionModels.create({ status, type }).save()
+    }
 
     // Insérer un utilisateur en BDD
     const createdUser = await UsersModels.create({
@@ -22,19 +46,19 @@ export class AuthResolver {
       username,
       email,
       password: hashedPassword,
-    }).save();
-
-    await SubscriptionModels.create({
-      user: createdUser
+      subscription
     }).save()
 
-    console.log("process.env.ACCESS_TOKEN_SECRET",process.env.ACCESS_TOKEN_SECRET)
+    console.log(
+      "process.env.ACCESS_TOKEN_SECRET",
+      process.env.ACCESS_TOKEN_SECRET
+    )
     const token = sign(
       { userId: createdUser.id },
-      process.env.ACCESS_TOKEN_SECRET ?? 'test-secret'
-    );
+      process.env.ACCESS_TOKEN_SECRET ?? "test-secret"
+    )
 
-    return token;
+    return token
   }
 
   @Query(() => String)
@@ -42,31 +66,51 @@ export class AuthResolver {
     @Arg("email") email: string,
     @Arg("password") password: string
   ): Promise<string> {
-    const userFoundByEmail = await UsersModels.findOne({ where: { email } });
+    const date = new Date()
+
+    const userFoundByEmail = await UsersModels.findOne({ where: { email } })
     if (userFoundByEmail == null) {
-      throw new Error("Invalid credentials");
+      throw new Error("Invalid credentials")
     }
 
+    if (userFoundByEmail.subscription === null) {
+      throw new Error("subscription not found")
+    }
+
+    if (
+      userFoundByEmail.subscription.status === "Active" &&
+      userFoundByEmail.subscription.subscriptionEndedAt <= date
+    ) {
+      const status = "Inactive"
+      const duration = ""
+      const type = "Free"
+      await SubscriptionModels.merge(userFoundByEmail.subscription, {
+        duration,
+        status,
+        subscribedAt: date,
+        type
+      }).save()
+    }
     const passwordValid: boolean = await argon2.verify(
       userFoundByEmail.password,
       password
-    );
+    )
     if (!passwordValid) {
-      throw new Error("Invalid credentials");
+      throw new Error("Invalid credentials")
     }
 
     const token = sign(
       { userId: userFoundByEmail.id },
-      process.env.ACCESS_TOKEN_SECRET ?? 'test-secret'
-    );
+      process.env.ACCESS_TOKEN_SECRET ?? "test-secret"
+    )
 
-    return token;
+    return token
   }
 
   @Authorized()
   @Query(() => String)
   async getProfile(@Ctx() context: any): Promise<string> {
-    console.log("user", context.user);
-    return context.user?.email;
+    console.log("user", context.user)
+    return context.user?.email
   }
 }
