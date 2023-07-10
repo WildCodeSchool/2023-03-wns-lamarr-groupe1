fdf8b1f7ce2f24a6487c335379b8b9d7e972e418
romain ROGER <romain32.roger@gmail.com>
2023-07-10 15:47:42 +0200

    MàJ Home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-<<<<<<< HEAD
 import "../styles/Home.scss";
-=======
 import Layout from "../components/common/layouts/Layout";
->>>>>>> 9b4d4ed3164079679d2b09522daf500e8918f0f6
 
 const Home = () => {
   const navigate = useNavigate();
@@ -17,24 +14,13 @@ const Home = () => {
   };
 
   return (
-<<<<<<< HEAD
-    <div className="container-home">
-      <p className="hello">Hello i'm the homepage</p>
-      <button onClick={handleClickK}>Khemis</button>
-      <button onClick={handleClickR}>Romain</button>
-=======
-    <div>
-      <Layout>
-        <p>
-          Hello i'm the homepage ! I'll make my muscle grow up and i'll be back
-          <br></br>
-          Click beyond to see what Khemis, Flora & Romain has done
-        </p>
+    <Layout>
+      <div className="container-home">
+        <p className="hello">Hello i'm the homepage</p>
         <button onClick={handleClickK}>Khemis</button>
         <button onClick={handleClickR}>Romain</button>
-      </Layout>
->>>>>>> 9b4d4ed3164079679d2b09522daf500e8918f0f6
-    </div>
+      </div>
+    </Layout>
   );
 };
 
