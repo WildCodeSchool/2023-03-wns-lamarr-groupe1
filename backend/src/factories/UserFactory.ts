f434b7b255c649a17b32c1cbdc4a7c6f9de3e56e
Gautier Fondevila <g.fondevila@humansmatter.co>
2023-07-11 12:06:07 +0200

    remove commented code

--- backend/src/factories/UserFactory.ts
+++ backend/src/factories/UserFactory.ts
@@ -1,23 +1,3 @@
-// import { Faker } from "@faker-js/faker";
-// import { setSeederFactory } from "typeorm-extension";
-// import {UsersModels} from "../models/UsersModels";
-// import * as argon2 from "argon2"
-
-// export const UserFactory = setSeederFactory(UsersModels, async (faker: Faker) => {
-//   const user = new UsersModels();
-//   user.email = faker.internet.email();
-//   const password = faker.internet.password({
-//     length: 8,
-//     prefix: '@A'
-//   });
-//   user.password = await argon2.hash(password)
-//   user.username = faker.internet.userName();
-//   user.firstname = faker.person.firstName()
-//   user.lastname = faker.person.lastName()
-//   user.subscription = 
-//   return user
-// });
-
 import { Faker } from "@faker-js/faker";
 import { setSeederFactory } from "typeorm-extension";
 import { UsersModels } from "../models/UsersModels";
