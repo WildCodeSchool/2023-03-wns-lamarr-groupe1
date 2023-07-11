83a5ea229a9580623bb53201874a3caa5f4cc212
christopher cuzin <ed.ed1009@hotmail.fr>
2023-07-11 11:48:27 +0200

    WOF_home fixes for responsive

--- frontend/src/components/common/HomePage.tsx
+++ frontend/src/components/common/HomePage.tsx
@@ -5,27 +5,28 @@ import { Link } from "react-router-dom"
 
 const HomePage = () => {
   return (
-    <div className="home">
-      <div className="img-container">
-        <img src={img} alt="Illustration page d'accueil" />
-      </div>
-      <div id="Accueil" className="container">
-        <div className="content-container">
-          <h2 className="main-title">
-            coder avec
-            <br /> <span>style !</span>
-          </h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            rhoncus.commodo velit. Vivamus iaculis varius accumsan. Nunc
-          </p>
-          <div className="link-container">
-            <Link to="sign-up">
-              <button className="button-start">Commencer</button>
-            </Link>
-            <Link className="learn-more" to="#Pricing">
-              En savoir plus
-            </Link>
+    <div className="home-main-container">
+      <h1>CreativeCode</h1>
+      <div id="Accueil" className="home">
+        <div className="img-container">
+          <img src={img} alt="Illustration page d'accueil" />
+        </div>
+        <div className="container">
+          <div className="content-container">
+            <h2 className="main-title">Le code a du style avec CreativeCode !</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum rhoncus.commodo velit. Vivamus iaculis varius accumsan.
+              Nunc
+            </p>
+            <div className="link-container">
+              <Link to="sign-up">
+                <button className="button-start">Commencer</button>
+              </Link>
+              <Link className="learn-more" to="#Pricing">
+                En savoir plus
+              </Link>
+            </div>
           </div>
         </div>
       </div>
