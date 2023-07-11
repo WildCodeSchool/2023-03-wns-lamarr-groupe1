7e529da797a0576a8033e1b5fe19956de37c67a3
romain ROGER <romain32.roger@gmail.com>
2023-07-11 09:04:23 +0200

    suppr button

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,26 +1,14 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import "../styles/Home.scss";
 import Layout from "../components/common/layouts/Layout";
 import AboutUs from "../components/common/AboutUs";
 import "../styles/AboutUs.scss";
 const Home = () => {
-  const navigate = useNavigate();
-
-  const handleClickR = () => {
-    navigate("/subscribe");
-  };
-  const handleClickK = () => {
-    navigate("/sign-in");
-  };
-
   return (
     <>
       <Layout>
         <div className="container-home">
           <p className="hello">Hello i'm the homepage</p>
-          <button onClick={handleClickK}>Khemis</button>
-          <button onClick={handleClickR}>Romain</button>
         </div>
       </Layout>
     </>
