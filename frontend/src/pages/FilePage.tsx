6a2b3bdc53295616f8a5c621a49c6ed7d2ced520
christopher <ed.ed1009@hotmail.fr>
2023-10-04 17:25:42 +0200

    WOF_restriction_accés add hoc authenticatedPage to restrict access to pages if user is not connected

--- frontend/src/pages/FilePage.tsx
+++ frontend/src/pages/FilePage.tsx
@@ -1,30 +1,30 @@
-import React from "react";
-import Layout from "components/common/layouts/Layout";
-import GridFile from "components/common/GridFile";
-import "styles/FilePage.scss";
-import { useContext } from "react";
-import { dataFile } from "utils/dataFile";
-import { fileContext } from "utils/context/FileContext";
-import FormNewFile from "components/common/form/FormAddFile";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import "styles/AddFileForm.scss";
+import Layout from "components/common/layouts/Layout"
+import GridFile from "components/common/GridFile"
+import "styles/FilePage.scss"
+import { useContext } from "react"
+import { dataFile } from "utils/dataFile"
+import { fileContext } from "utils/context/FileContext"
+import FormNewFile from "components/common/form/FormAddFile"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPlus } from "@fortawesome/free-solid-svg-icons"
+import "styles/AddFileForm.scss"
+import AuthenticatedPage from "utils/hoc/authenticatedPage"
 // src/types/file.ts
 // Dedans, on va exporter le type suivant :
 type File = {
-  id: number;
-  filename: string;
-  content: string;
-  createdAt: string;
-  image: string;
-  isPublic: boolean;
-};
+  id: number
+  filename: string
+  content: string
+  createdAt: string
+  image: string
+  isPublic: boolean
+}
 
 const FilePage = () => {
-  const privateFiles: File[] = dataFile.filter((file) => !file.isPublic);
-  const publicFiles: File[] = dataFile.filter((file) => file.isPublic);
+  const privateFiles: File[] = dataFile.filter((file) => !file.isPublic)
+  const publicFiles: File[] = dataFile.filter((file) => file.isPublic)
 
-  const { isShow, handleOpenModal, handleCloseModal } = useContext(fileContext);
+  const { isShow, handleOpenModal, handleCloseModal } = useContext(fileContext)
 
   return (
     <Layout>
@@ -54,7 +54,7 @@ const FilePage = () => {
         </>
       ) : null}
     </Layout>
-  );
-};
+  )
+}
 
-export default FilePage;
+export default AuthenticatedPage(FilePage)
