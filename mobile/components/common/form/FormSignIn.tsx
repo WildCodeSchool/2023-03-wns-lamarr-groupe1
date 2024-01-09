802f50477fdd6a17098b5867d294573dd7a16c80
Khemis-Louison <khemis.louison@digitaweb.com>
2024-01-09 15:35:22 +0100

    page signIn

--- mobile/components/common/form/FormSignIn.tsx
+++ mobile/components/common/form/FormSignIn.tsx
@@ -3,7 +3,15 @@ import { SIGN_IN_QUERY } from "../../../graphql/queries/SIGN_IN_QUERY";
 import { useLazyQuery } from "@apollo/client";
 import { useForm, Controller, SubmitHandler } from "react-hook-form";
 import { ErrorMessage } from "@hookform/error-message";
-import { Text, View, Image, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface IuserSignIn {
@@ -12,8 +20,8 @@ interface IuserSignIn {
 }
 
 const FormSignIn = ({ navigation }) => {
-  const [credantials, setCredantials] = useState(false);
   const [signIn, { loading }] = useLazyQuery(SIGN_IN_QUERY);
+  const [toast, setToast] = useState(false);
 
   const {
     control,
@@ -27,102 +35,219 @@ const FormSignIn = ({ navigation }) => {
       });
       const token = result.data.signIn;
       await AsyncStorage.setItem("token", token);
-      setCredantials(false);
-      navigation.navigate("Home");
-      console.log(result.data.signIn);
+      setTimeout(() => {
+        setToast(true);
+        setTimeout(() => {
+          setToast(false);
+          navigation.navigate("Home");
+        }, 3000);
+      }, 1000);
     } catch (error) {
       console.log(error);
-      setCredantials(true);
     }
   };
 
   return (
-    <View>
-      <Text>Connectez-vous</Text>
-      <Text>Bonjour ! Renseigner vos coordonnées pour vous connecter</Text>
-      {credantials ? (
-        <View>
-          <Text>l'email ou le mot de passe n'est pas valide</Text>
+    <View style={[styles.displayFlexCenter]}>
+      <View style={[styles.containerCard, { backgroundColor: "#5340a9" }]}>
+        <View style={[{ width: "100%" }, { paddingHorizontal: 20 }]}>
+          <Text
+            style={[styles.title, styles.textAlignCenter, { color: "#fff" }]}
+          >
+            Connectez-vous
+          </Text>
+          <Text
+            style={[styles.text, styles.textAlignCenter, { color: "#fff" }]}
+          >
+            Bonjour ! Renseigner vos coordonnées pour vous connecter
+          </Text>
+          {toast && (
+            <View>
+              <Text style={[styles.success, styles.textAlignCenter]}>
+                Inscription réussie! Vous allez être redirigé vers la page
+                d'accueil.
+              </Text>
+            </View>
+          )}
+          <Controller
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={styles.input}
+                id="email"
+                placeholder="Email*"
+                onBlur={onBlur}
+                onChangeText={(value) => onChange(value)}
+                value={value}
+              />
+            )}
+            name="email"
+            rules={{
+              required: true,
+              pattern: {
+                value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+                message: "Format d'email invalide",
+              },
+            }}
+          />
+
+          <ErrorMessage
+            errors={errors}
+            name="email"
+            render={({ message }) => (
+              <Text style={styles.error}> {message}</Text>
+            )}
+          />
+          <Controller
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={styles.input}
+                id="password"
+                placeholder="Mot de passe*"
+                onBlur={onBlur}
+                onChangeText={(value) => onChange(value)}
+                value={value}
+                secureTextEntry={true}
+              />
+            )}
+            name="password"
+            rules={{
+              required: true,
+              minLength: {
+                value: 8,
+                message: "Le mot de passe doit contenir au moins 8 caractères",
+              },
+              pattern: {
+                value: /^(?=.*[A-Z])(?=.*[!@#$%^&*])/,
+                message:
+                  "Le mot de passe doit contenir une lettres majuscules et minuscules, un chiffres et un caractères spécial",
+              },
+            }}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="password"
+            render={({ message }) => (
+              <Text style={styles.error}> {message}</Text>
+            )}
+          />
+
+          <TouchableOpacity
+            style={[styles.customButton, { backgroundColor: "#fff" }]}
+            onPress={handleSubmit(onSubmit)}
+            disabled={loading}
+          >
+            <Text style={(styles.buttonText, { color: "#5340a9" })}>
+              Se connecter
+            </Text>
+          </TouchableOpacity>
         </View>
-      ) : null}
 
-      <View>
-        <Controller
-          control={control}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              id="email"
-              placeholder="Email*"
-              onBlur={onBlur}
-              onChangeText={(value) => onChange(value)}
-              value={value}
-            />
-          )}
-          name="email"
-          rules={{
-            required: true,
-            pattern: {
-              value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-              message: "Format d'email invalide",
-            },
-          }}
-        />
-
-        <ErrorMessage
-          errors={errors}
-          name="email"
-          render={({ message }) => <Text> {message}</Text>}
-        />
-        <Controller
-          control={control}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              id="password"
-              placeholder="Mot de passe*"
-              onBlur={onBlur}
-              onChangeText={(value) => onChange(value)}
-              value={value}
-            />
-          )}
-          name="password"
-          rules={{
-            required: true,
-            minLength: {
-              value: 8,
-              message: "Le mot de passe doit contenir au moins 8 caractères",
-            },
-            pattern: {
-              value: /^(?=.*[A-Z])(?=.*[!@#$%^&*])/,
-              message:
-                "Le mot de passe doit contenir une lettres majuscules et minuscules, un chiffres et un caractères spécial",
-            },
-          }}
-        />
-        <ErrorMessage
-          errors={errors}
-          name="password"
-          render={({ message }) => <Text> {message}</Text>}
-        />
-        <Button
-          title="Se connecter"
-          onPress={handleSubmit(onSubmit)}
-          disabled={loading}
-        />
-      </View>
-      <Text>- OR -</Text>
-
-      <View>
-        <Button title="Connection avec Google" />
-        <Button title="Connection avec Facebook" />
-      </View>
-      <Text>
-        Pas encore de compte ?{" "}
-        <Text onPress={() => navigation.navigate("File")}>
-          Inscrivez-vous !
+        <Text style={[styles.text, styles.textAlignCenter, { color: "#fff" }]}>
+          Pas encore de compte ?{" "}
+          <Text
+            style={[styles.text, styles.textAlignCenter, { color: "#849BCE" }]}
+            onPress={() => navigation.navigate("File")}
+          >
+            Inscrivez-vous !
+          </Text>
         </Text>
-      </Text>
+      </View>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  marginSection: {
+    marginBottom: 80,
+  },
+  textAlignCenter: {
+    textAlign: "center",
+  },
+  title: {
+    fontWeight: "600",
+    fontSize: 25,
+    marginBottom: 20,
+    textTransform: "uppercase",
+  },
+  text: {
+    fontWeight: "600",
+    marginBottom: 20,
+  },
+  displayFlexCenter: {
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  containerCard: {
+    position: "relative",
+    marginVertical: 30,
+    marginHorizontal: 15,
+    paddingHorizontal: 0,
+    paddingVertical: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.75,
+    shadowRadius: 17,
+    elevation: 5,
+    overflow: "hidden",
+  },
 
+  customButton: {
+    alignItems: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#5340a9",
+    backgroundColor: "#5340a9",
+    marginVertical: 15,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "500",
+  },
+
+  input: {
+    marginVertical: 10, // margin: 20px auto 10px; en React Native, utilisez marginVertical pour le margin-top et le margin-bottom // En React Native, il n'y a pas de valeur 'auto' pour la marge horizontale
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderColor: "#5340a9",
+    fontWeight: "bold",
+    paddingVertical: 8,
+    paddingHorizontal: 18,
+    borderRadius: 5,
+    color: "#222",
+  },
+  error: {
+    marginVertical: 0, // margin: 20px auto 10px; en React Native, utilisez marginVertical pour le margin-top et le margin-bottom // En React Native, il n'y a pas de valeur 'auto' pour la marge horizontale
+    backgroundColor: "pink",
+    borderWidth: 1,
+    borderColor: "#5340a9",
+    fontWeight: "bold",
+    paddingVertical: 8,
+    paddingHorizontal: 18,
+    borderRadius: 5,
+    color: "red",
+  },
+  success: {
+    marginVertical: 0, // margin: 20px auto 10px; en React Native, utilisez marginVertical pour le margin-top et le margin-bottom // En React Native, il n'y a pas de valeur 'auto' pour la marge horizontale
+    backgroundColor: "green",
+    borderWidth: 1,
+    borderColor: "#5340a9",
+    fontWeight: "bold",
+    paddingVertical: 8,
+    paddingHorizontal: 18,
+    borderRadius: 5,
+    color: "white",
+  },
+});
 export default FormSignIn;
--- mobile/screens/SignInScreens.tsx
+++ mobile/screens/SignInScreens.tsx
@@ -3,14 +3,14 @@ import { StyleSheet, Text, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 const SignInScreens = () => {
-	const navigation = useNavigation();
-	return (
-		<>
-			<View>
-				<FormSignIn navigation={navigation} />
-			</View>
-		</>
-	);
+  const navigation = useNavigation();
+  return (
+    <>
+      <View style={[{ flex: 1 }, { backgroundColor: "#fff" }]}>
+        <FormSignIn navigation={navigation} />
+      </View>
+    </>
+  );
 };
 
 export default SignInScreens;
