3f982a6a8c4b02d49a372456a1be1990d55fd49a
Gautier Fondevila <g.fondevila@humansmatter.co>
2024-01-10 12:12:39 +0100

    merge with WOM_SigUp_Page and fix conflict

--- mobile/screens/SignUpScreens.tsx
+++ mobile/screens/SignUpScreens.tsx
@@ -2,12 +2,12 @@ import { StyleSheet, Text, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import FormSignUp from "../components/common/form/FormSignUp";
 
-const SignUpScreens = () => {
-  const navigation = useNavigation();
+const SignUpScreens = ({navigation, route}) => {
+  
   return (
     <>
       <View style={[{ flex: 1 }, { backgroundColor: "#fff" }]}>
-        <FormSignUp navigation={navigation} />
+        <FormSignUp navigation={navigation} route={route} />
       </View>
     </>
   );
